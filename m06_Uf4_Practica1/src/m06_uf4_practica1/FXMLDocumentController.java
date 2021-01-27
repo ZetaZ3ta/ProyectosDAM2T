dba5fabaad84bb85797899c5025ef8fdb9ab4993
Leandro Joseph Paredes Llerena <leandroparedes0721@gmail.com>
2021-01-27 17:37:27 +0100

    test

--- m06_Uf4_Practica1/src/m06_uf4_practica1/FXMLDocumentController.java
+++ m06_Uf4_Practica1/src/m06_uf4_practica1/FXMLDocumentController.java
@@ -24,7 +24,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Grupo   5");
+        label.setText("Grupo 5");
         
        
                 
