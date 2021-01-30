cb4bf455fd7f55e347134dd908f5d1095d8d38d4
ZetaZ3ta <71658714+ZetaZ3ta@users.noreply.github.com>
2021-01-30 19:04:27 +0100

    BBDD actualizada

--- QueryBBDD.sql
+++ QueryBBDD.sql
@@ -1,7 +1,8 @@
--- MySQL Workbench Forward Engineering
-
 DROP USER IF EXISTS 'TrioCalavera';
 CREATE USER 'TrioCalavera' IDENTIFIED BY 'ElTrioCalaverita';
+
+DROP DATABASE IF EXISTS m06uf4p1;
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
@@ -17,7 +18,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema M06Uf4P1
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `M06Uf4P1` DEFAULT CHARACTER SET latin1;
+CREATE SCHEMA IF NOT EXISTS `M06Uf4P1` DEFAULT CHARACTER SET latin1 ;
 USE `M06Uf4P1` ;
 
 -- -----------------------------------------------------
@@ -26,16 +27,17 @@ USE `M06Uf4P1` ;
 CREATE TABLE IF NOT EXISTS `M06Uf4P1`.`Vuelo` (
   `numVuelo` INT NOT NULL,
   `capacidad` INT NULL DEFAULT NULL,
-  `Fecha y Hora` TIMESTAMP NOT NULL,
+  `fechaHora` TIMESTAMP NOT NULL,
   PRIMARY KEY (`numVuelo`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `M06Uf4P1`.`Asiento`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `M06Uf4P1`.`Asiento` (
-  `idAsiento` VARCHAR(4) NOT NULL,
+  `idAsiento` VARCHAR(4) CHARACTER SET 'latin1' NOT NULL,
   `numVuelo` INT NOT NULL,
   `Lleno` TINYINT(1) NOT NULL,
   PRIMARY KEY (`idAsiento`),
@@ -45,7 +47,8 @@ CREATE TABLE IF NOT EXISTS `M06Uf4P1`.`Asiento` (
     REFERENCES `M06Uf4P1`.`Vuelo` (`numVuelo`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
@@ -71,10 +74,14 @@ CREATE TABLE IF NOT EXISTS `M06Uf4P1`.`Pasajero` (
     REFERENCES `M06Uf4P1`.`Asiento` (`idAsiento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 INSERT INTO vuelo VALUES (1,100,NOW());
+
+SELECT * FROM vuelo;
